6a9b24b5f54e831589d5b159e92f06575d4a9239
ryan wong <wong.ryan5091@gmail.com>
2024-11-05 13:09:09 +0800

    commit

--- Prog_fun/Prog_fun/cms.c
+++ Prog_fun/Prog_fun/cms.c
@@ -3,27 +3,30 @@
 #include <string.h>
 #include <ctype.h>
 
-#define MAX_STUDENTS 100 // Define a maximum number of students
 #define FILE_PATH "database.txt"
 #define USERNAME "CMS"
-#define TABLE_NAME_LENGTH 30
-#define HASHMAP_SIZE 101 // A prime number for better distribution
+#define TABLE_NAME_LENGTH 15
+#define HASHMAP_SIZE 103 // table size is a prime number which helps reduce collisions
+#define STUDENT_NAME_LENGTH 30
+#define PROGRAMME_LENGTH 30
 
-char tableName[TABLE_NAME_LENGTH] = ""; // Global tableName variable
+// Store the table name
+char tableName[TABLE_NAME_LENGTH] = "";
 
 typedef struct StudentRecords {
     int id;
-    char name[30];
-    char programme[30];
+    char name[STUDENT_NAME_LENGTH];
+    char programme[PROGRAMME_LENGTH];
     float mark;
-    struct StudentRecords* next; // Pointer for linked list
+    struct StudentRecords* next; // Pointer to next node for preventing hash colision
 } StudentRecords;
 
 typedef struct HashMap {
     StudentRecords* table[HASHMAP_SIZE]; // Array of pointers to StudentRecords
 } HashMap;
 
-// Hash function to compute the index based on student ID
+// Perform hashing which the result will be between 0 and hashmap size - 1
+// Using prime number for tabel size means that modulo operation will map the different keys to different slots more evenly in the hash map which reduce collision
 unsigned int hash(int id) {
     return id % HASHMAP_SIZE;
 }
@@ -32,7 +35,6 @@ unsigned int hash(int id) {
 StudentRecords* createStudent(int id, const char* name, const char* programme, float mark) {
     StudentRecords* newStudent = malloc(sizeof(StudentRecords));
     newStudent->id = id;
-    // Correct usage of strncpy_s
     strncpy_s(newStudent->name, sizeof(newStudent->name), name, _TRUNCATE);
     strncpy_s(newStudent->programme, sizeof(newStudent->programme), programme, _TRUNCATE);
     newStudent->mark = mark;
